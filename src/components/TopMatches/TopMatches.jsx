af5009fb4acc958e66a69f883f0c52aa8ab38015
Timchin-cpu <83727119+Timchin-cpu@users.noreply.github.com>
2025-01-21 18:45:04 +0000

    modified:   src/components/TopMatches/TopMatches.jsx

--- src/components/TopMatches/TopMatches.jsx
+++ src/components/TopMatches/TopMatches.jsx
@@ -26,6 +26,7 @@ function TopMatches() {
               ...prevData,
               [sportName]: data,
             }));
+            console.log(sportData);
           }
         }
       } catch (error) {
