9639b021c3f78ce4b1295573b7018b5696666b2c
Timchin-cpu <83727119+Timchin-cpu@users.noreply.github.com>
2025-01-21 17:30:07 +0000

    modified:   src/components/SupportFighters/SupportFighters.jsx 	modified:   src/components/TopFan/TopFan.jsx 	modified:   src/components/TopFighters/TopFighters.jsx 	modified:   src/components/TopMatches/TopMatches.jsx

--- src/components/SupportFighters/SupportFighters.jsx
+++ src/components/SupportFighters/SupportFighters.jsx
@@ -6,21 +6,35 @@ function SupportFighters() {
       <div className={styles.container}>
         <div className={styles.topBar}>
           <div className={styles.backArrow}>
-            <img src="arrow.png" alt="#" />
-            <h1>SportDonation</h1>
+            <img src="arrow.png" alt="#" onClick={() => navigate("/main")} />
+            <h1>BroDonate</h1>
           </div>
           <div className={styles.iconsContainer}>
             <img
               src="Notification.png"
               alt=""
               className={styles.notification}
+              onClick={() => {
+                navigate("/Notifications");
+              }}
             />
-            <img src="search.png" alt="" className={styles.search} />
+            <img
+              src="search.png"
+              alt=""
+              className={styles.search}
+              onClick={() => navigate("/Saerch")}
+            />
           </div>
         </div>
       </div>
+      BroDonate{" "}
       <div className={styles.bottomNav}>
-        <div className={styles.catalogItem}>
+        <div
+          className={styles.catalogItem}
+          onClick={() => {
+            navigate("/main");
+          }}
+        >
           <img
             src="ui-checks-grid.png"
             alt=""
@@ -28,7 +42,12 @@ function SupportFighters() {
           />
           <p className={styles.catalogText}>Каталог</p>
         </div>
-        <div className={styles.catalogItem}>
+        <div
+          className={styles.catalogItem}
+          onClick={() => {
+            navigate("/alltournaments");
+          }}
+        >
           <img
             src="lightning-charge.png"
             alt=""
@@ -36,11 +55,25 @@ function SupportFighters() {
           />
           <p className={styles.catalogText}>Турниры</p>
         </div>
-        <div className={styles.catalogItem}>
+        <div
+          className={styles.catalogItem}
+          onClick={() => {
+            navigate("/Referal");
+          }}
+        >
           <img src="gift.png" alt="" className={styles.catalogImage} />
           <p className={styles.catalogText}>Рефералы</p>
         </div>
-        <div className={styles.catalogItem}>
+        <div
+          className={styles.catalogItem}
+          onClick={() => {
+            if (userType === "fan") {
+              navigate("/profileuser");
+            } else {
+              navigate("/profilefighter");
+            }
+          }}
+        >
           <img src="person.png" alt="" className={styles.catalogImage} />
           <p className={styles.catalogText}>Профиль</p>
         </div>
