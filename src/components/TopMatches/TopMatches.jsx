edbfdc0eb2a16066807cb81008e07de7370668af
Timchin-cpu <83727119+Timchin-cpu@users.noreply.github.com>
2025-01-21 18:58:15 +0000

    modified:   src/components/TopMatches/TopMatches.jsx

--- src/components/TopMatches/TopMatches.jsx
+++ src/components/TopMatches/TopMatches.jsx
@@ -6,6 +6,9 @@ function TopMatches() {
   const userType = localStorage.getItem("userType");
   const navigate = useNavigate();
   const [sportData, setsportData] = useState({});
+  const tournaments = sportData.tournaments;
+  const matches = sportData.matches;
+
   const sports = useMemo(
     () => [
       "ММА",
@@ -65,7 +68,39 @@ function TopMatches() {
           </div>
         </div>
       </div>
-
+      {tournaments.map((tournament) => (
+        <div key={tournament.id}>
+          <div className={styles.competitions}>
+            <div className={styles.competitionsText}>
+              <p>{tournament.name}</p>
+            </div>
+            <img
+              src={isOpen[tournament.id] ? "up.png" : "down.png"}
+              alt=""
+              onClick={() => toggleOpen(tournament.id)}
+              style={{ cursor: "pointer" }}
+            />
+          </div>
+          {isOpen[tournament.id] && (
+            <div
+              onClick={() => handleMatchClick(tournament)}
+              style={{ cursor: "pointer" }}
+            >
+              {matches
+                .filter((match) => match.tournament_id === tournament.id)
+                .map((match) => (
+                  <div key={match.id} className={styles.dropdownContent}>
+                    <p className={styles.name}>{match.competitor_1}</p>
+                    <div className={styles.timeDate}>
+                      <p>{match.match_date}</p>
+                    </div>
+                    <p className={styles.name}>{match.competitor_2}</p>
+                  </div>
+                ))}
+            </div>
+          )}
+        </div>
+      ))}
       <div className={styles.bottomNav}>
         <div
           className={styles.catalogItem}
