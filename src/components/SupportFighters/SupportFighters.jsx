ab82b36ddc9a0aca0de3dac340a25bb40ca6b68d
ArtFullstack <ve4tom@gmail.com>
2025-01-26 15:05:30 +0000

    modified:   src/components/SupportFighters/SupportFighters.jsx

--- src/components/SupportFighters/SupportFighters.jsx
+++ src/components/SupportFighters/SupportFighters.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./SupportFighters.module.css";
 import { useNavigate, useLocation } from "react-router-dom";
 
