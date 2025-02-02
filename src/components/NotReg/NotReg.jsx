4ccc3c4aef21a42af2d0134dd0b01edaa21c2b51
ArtFullstack <ve4tom@gmail.com>
2025-02-02 16:44:26 +0000

    new file:   src/components/NotReg/NotReg.jsx 	new file:   src/components/NotReg/NotReg.module.css

---
+++ src/components/NotReg/NotReg.jsx
@@ -0,0 +1,138 @@
+import React from "react";
+import styles from "./NotReg.module.css";
+import { useNavigate } from "react-router-dom";
+
+function NotReg() {
+  const navigate = useNavigate();
+  const userType = localStorage.getItem("userType");
+  const userId = localStorage.getItem("userId");
+
+  return (
+    <div className={styles.header}>
+      <div className={styles.container}>
+        <div className={styles.topBar}>
+          <div className={styles.backArrow}>
+            <img src="arrow.png" alt="#" onClick={() => navigate(-1)} />
+            <h1>BroDonate</h1>
+          </div>
+          <div className={styles.iconsContainer}>
+            <img
+              src="Notification.png"
+              alt=""
+              className={styles.notification}
+              onClick={() => {
+                navigate("/Notifications");
+              }}
+            />
+            <img
+              src="search.png"
+              alt=""
+              className={styles.search}
+              onClick={() => navigate("/Saerch")}
+            />
+          </div>
+        </div>
+        <p>Для просмотра выполните вход в аккаунт или зарегигистрируйтесь</p>
+      </div>
+      <div className={styles.bottomNav}>
+        <div
+          className={styles.catalogItem}
+          onClick={() => {
+            navigate("/");
+            setActiveTab("catalog");
+          }}
+        >
+          <img
+            src={
+              activeTab === "catalog"
+                ? "ui-checks-grid-black.png"
+                : "ui-checks-grid.png"
+            }
+            alt=""
+            className={styles.catalogImage}
+          />
+          <p
+            className={`${styles.catalogText} ${
+              activeTab === "catalog" ? styles.activeText : ""
+            }`}
+          >
+            Главная
+          </p>
+        </div>
+
+        <div
+          className={styles.catalogItem}
+          onClick={() => {
+            navigate("/alltournaments");
+            setActiveTab("tournaments");
+          }}
+        >
+          <img
+            src={
+              activeTab === "tournaments"
+                ? "lightning-charge-black.png"
+                : "lightning-charge.png"
+            }
+            alt=""
+            className={styles.catalogImage}
+          />
+          <p
+            className={`${styles.catalogText} ${
+              activeTab === "tournaments" ? styles.activeText : ""
+            }`}
+          >
+            Турниры
+          </p>
+        </div>
+
+        <div
+          className={styles.catalogItem}
+          onClick={() => {
+            navigate("/Referal");
+            setActiveTab("referrals");
+          }}
+        >
+          <img
+            src={activeTab === "referrals" ? "gift-black.png" : "gift.png"}
+            alt=""
+            className={styles.catalogImage}
+          />
+          <p
+            className={`${styles.catalogText} ${
+              activeTab === "referrals" ? styles.activeText : ""
+            }`}
+          >
+            Рефералы
+          </p>
+        </div>
+
+        <div
+          className={styles.catalogItem}
+          onClick={() => {
+            if (userType === "fan") {
+              navigate("/profileuser");
+            } else {
+              navigate("/profilefighter");
+            }
+            setActiveTab("profile");
+          }}
+        >
+          <img
+            src={activeTab === "profile" ? "person-black.png" : "person.png"}
+            alt=""
+            className={styles.catalogImage}
+          />
+          <p
+            className={`${styles.catalogText} ${
+              activeTab === "profile" ? styles.activeText : ""
+            }`}
+          >
+            Профиль
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default NotReg;
