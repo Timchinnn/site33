dbc84bc6067553ec522b7c3f643fa57fe54c1be0
ArtFullstack <ve4tom@gmail.com>
2025-02-02 17:01:00 +0000

    modified:   src/components/ProfileFighter/ProfileFighter.jsx

--- src/components/ProfileFighter/ProfileFighter.jsx
+++ src/components/ProfileFighter/ProfileFighter.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import styles from "./ProfileFighter.module.css";
 import { useNavigate } from "react-router-dom";
 
