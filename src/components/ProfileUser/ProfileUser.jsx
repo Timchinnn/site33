a2b4196b8d70e3b3fa4e6fcb487262a5739736b7
Timchin-cpu <83727119+Timchin-cpu@users.noreply.github.com>
2025-02-05 09:10:12 +0000

    modified:   src/components/ProfileUser/ProfileUser.jsx

--- src/components/ProfileUser/ProfileUser.jsx
+++ src/components/ProfileUser/ProfileUser.jsx
@@ -158,7 +158,7 @@ function ProfileUser() {
             <img src="layout-text-window.png" alt="" />
             <p>Помощь</p>
           </div>
-          {userId === "3" && userType === "fan" && (
+          {userId === "1" && userType === "fan" && (
             <div onClick={() => navigate("/AdminPanel")}>
               <img src="admin-icon.png" alt="" />
               <p>Админ панель</p>
