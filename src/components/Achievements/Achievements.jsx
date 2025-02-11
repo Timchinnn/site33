a17d957a4f55e92d8e00ce404b7860ceded5c06d
Timchinnn <95688018+Timchinnn@users.noreply.github.com>
2025-02-11 20:02:52 +0000

    modified:   src/components/Achievements/Achievements.jsx

--- src/components/Achievements/Achievements.jsx
+++ src/components/Achievements/Achievements.jsx
@@ -8,24 +8,11 @@ function Achievements() {
     const fetchAchievements = async () => {
       const userId = localStorage.getItem("userId");
       try {
-        const [votingResponse, achievementsResponse] = await Promise.all([
-          fetch(`/api/voting-achievements/${userId}`),
-          fetch(`/api/achievements/${userId}`),
-        ]);
-
-        if (votingResponse.ok && achievementsResponse.ok) {
-          const votingData = await votingResponse.json();
-          const achievementsData = await achievementsResponse.json();
-
-          // Объединяем данные из обоих запросов
-          const combinedData = {
-            ...votingData,
-            ...achievementsData,
-            scoutProgress: achievementsData?.scoutProgress,
-          };
-
-          setAchievements(combinedData);
-          console.log(achievementsData?.scoutProgress.current);
+        const response = await fetch(`/api/achievements/${userId}`);
+        if (response.ok) {
+          const data = await response.json();
+          console.log(data?.scoutProgress.current);
+          setAchievements(data);
         }
       } catch (error) {
         console.error("Error fetching achievements:", error);
@@ -34,6 +21,7 @@ function Achievements() {
 
     fetchAchievements();
   }, []);
+
   const navigate = useNavigate();
   const userType = localStorage.getItem("userType");
   const userId = localStorage.getItem("userId");
